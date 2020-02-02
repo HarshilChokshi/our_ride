04db6ebca0bc2994a16c1b57e37ce2ce2c7e88b3
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2020-02-02 18:25:55 -0500

    Added screen for signing up

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:our_ride/src/screens/sign_up_screen.dart';
 import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'OURide',
-      home: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/background.png'), fit: BoxFit.cover)),
-        child: new Scaffold(
-            backgroundColor: Colors.transparent,
-            resizeToAvoidBottomPadding: false,
-            body: new LoginScreen()
-          ),
-      )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+      },
     );
   }
 }
